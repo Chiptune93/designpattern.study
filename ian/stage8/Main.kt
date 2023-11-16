6db6d72bff1d43e2ef051a29a8d8032cad718884
zlwkald <zlwkadl@gmail.com>
2023-11-17 01:43:57 +0900

    stage8

---
+++ ian/stage8/Main.kt
@@ -0,0 +1,8 @@
+
+fun main(args: Array<String>) {
+    val youName : PermissionCheck = YourName()
+    println("ian.curtis")
+    youName.check("ian.curtis")
+    println("harry.93")
+    youName.check("harry.93")
+}
